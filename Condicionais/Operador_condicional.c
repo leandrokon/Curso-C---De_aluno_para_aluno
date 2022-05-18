502203b5882200a5652f31b2dd012674ca8e7542
Leandro Konrath <leandro0.konrath@gmail.com>
2022-05-18 10:02:13 -0300

    Operador_condicional

---
+++ Condicionais/Operador_condicional.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int num1 = 10;
+    int num2 = 50;
+
+    // ? -> SE
+    // : -> SENÂO 
+
+//num1 é menor que num2?                    //se não escreve essa msg.
+    num1 < num2 ? printf("num1 e menor!") : printf("num1 e maior!");
+                // se sim escreve essa msg
+    return 0;
+}
